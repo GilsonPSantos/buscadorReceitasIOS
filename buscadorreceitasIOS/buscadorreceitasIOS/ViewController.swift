bbb783c0ce07c53ce09bf1e356bece3746939717
gilsonsantos <gilsonsantos@gilsonsantoss-iMac.local>
2017-09-01 08:36:30 -0300

    primeiro comia

---
+++ buscadorreceitasIOS/buscadorreceitasIOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  buscadorreceitasIOS
+//
+//  Created by gilsonsantos on 01/09/17.
+//  Copyright © 2017 gilsonsantos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
