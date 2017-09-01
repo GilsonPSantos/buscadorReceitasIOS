16526be367101a34bdbe1a21a48e2054c58493c0
gilsonsantos <gilsonsantos@gilsonsantoss-iMac.local>
2017-09-01 08:51:55 -0300

    teste

---
+++ buscadorreceitasIOS/buscadorreceitasIOS/model/cardapio.swift
@@ -0,0 +1,13 @@
+//
+//  cardapio.swift
+//  buscadorreceitasIOS
+//
+//  Created by gilsonsantos on 01/09/17.
+//  Copyright © 2017 gilsonsantos. All rights reserved.
+//
+
+import UIKit
+
+class cardapio: NSObject {
+
+}
